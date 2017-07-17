4688f7fe51adbaf62b29e0c694e2165e8bb68ddb
Doug <dougmay17@gmail.com>
2017-07-16 18:48:12 -0600

    fixed header js

---
+++ js/projects.js
@@ -0,0 +1,8 @@
+$(window).on("scroll", function() {
+        if($(window).scrollTop() > 0) {
+            $("#header").addClass("darkHeader");
+        } else {
+
+           $("#header").removeClass("darkHeader");
+        }
+    });
